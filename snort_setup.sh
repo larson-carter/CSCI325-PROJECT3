5405fdf51bf61c9f4280ea39649923ad452ff2b7
Larson Carter <larsoncarter72404@gmail.com>
2024-11-13 14:55:39 -0600

    Fix SNORT Rules Install
    I fixed the snort rules install and what directory that it was installing the rules into.

--- snort_setup.sh
+++ snort_setup.sh
@@ -12,11 +12,14 @@ cd /tmp
 # Download community rules
 wget https://www.snort.org/downloads/community/community-rules.tar.gz -O community-rules.tar.gz
 
-# Extract the rules to the snort rules directory
-sudo tar -xzf community-rules.tar.gz -C /etc/snort/rules
+# Extract the rules directly into /etc/snort/rules, removing the leading directory
+sudo tar --strip-components=1 -xzf community-rules.tar.gz -C /etc/snort/rules
+
+# Clean up the downloaded archive
+rm community-rules.tar.gz
 
 # Include the community rules in snort.conf if not already included
-if ! grep -q "include \$RULE_PATH/community.rules" /etc/snort/snort.conf; then
+if ! sudo grep -Fxq "include \$RULE_PATH/community.rules" /etc/snort/snort.conf; then
     echo "include \$RULE_PATH/community.rules" | sudo tee -a /etc/snort/snort.conf
 fi
 
