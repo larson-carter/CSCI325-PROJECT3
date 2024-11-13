fee39eb9ec6037a345e3ad5558bd6631a6bb1688
Larson Carter <larsoncarter72404@gmail.com>
2024-11-13 14:45:30 -0600

    UFW DNS Fix
    I fixed the port of DNS for UFW.

--- ufw_setup.sh
+++ ufw_setup.sh
@@ -7,16 +7,16 @@ sudo ufw enable
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 
-# Allow specified services
-sudo ufw allow http       # HTTP (port 80)
-sudo ufw allow https      # HTTPS (port 443)
-sudo ufw allow dns        # DNS (port 53)
-sudo ufw allow ssh        # SSH (port 22)
-sudo ufw allow ftp        # FTP (port 21)
-sudo ufw allow smtp       # SMTP (port 25)
-sudo ufw allow 67/udp     # DHCP Server (port 67)
-sudo ufw allow 68/udp     # DHCP Client (port 68)
-sudo ufw allow imap       # IMAP (port 143)
+# Allow specified services by specifying ports and protocols
+sudo ufw allow 443/tcp      # HTTPS (port 443)
+sudo ufw allow 80/tcp       # HTTP (port 80)
+sudo ufw allow 53           # DNS (port 53 TCP and UDP)
+sudo ufw allow 22/tcp       # SSH (port 22)
+sudo ufw allow 21/tcp       # FTP (port 21)
+sudo ufw allow 25/tcp       # SMTP (port 25)
+sudo ufw allow 67/udp       # DHCP Server (port 67)
+sudo ufw allow 68/udp       # DHCP Client (port 68)
+sudo ufw allow 143/tcp      # IMAP (port 143)
 
 # Reload ufw to apply changes
 sudo ufw reload
